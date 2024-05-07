84f8efdcbd925ab213813d401b9dbfd0f07990c7
manuela montoya <you@example.com>
2024-05-07 09:28:42 -0500

    se realiza examenn

--- src/main/java/org/example/Familia.java
+++ src/main/java/org/example/Familia.java
@@ -31,6 +31,7 @@ public class Familia {
             Animal hijo= new Animal();
             hijo.nombre=nombre;
             hijo.peso=1;
+
             double random = Math.random();
             if(random<0.5){
                 hijo.genero="F";
@@ -39,8 +40,6 @@ public class Familia {
             }
             hijo=hijo;
 
-        }else{
-            System.out.println("Aun no puedes tener hijos");
         }
 
     }
